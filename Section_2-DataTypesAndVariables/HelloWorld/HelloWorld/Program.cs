a20061654484cc5d9ab04a8ff35d967a8b05a089
Amir Harrison <76721355+aharri64@users.noreply.github.com>
2021-09-18 09:46:04 -0600

    datatypes int, float, double

---
+++ Section_2-DataTypesAndVariables/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int num1 = 13;
+            int num2 = 5;
+            int sum = num1 + num2;
+
+            double d1 = 3.5;
+            double d2 = 1.337;
+            double sumD = d1 / d2;
+
+            Console.WriteLine("The sum of " + num1 + " and " + num2 + " is " + sum);
+            Console.WriteLine(d1 + " devided by " + num2 + " is " + sumD);
+            Console.Read();
+        }
+        
+    }
+}
