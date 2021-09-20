8914511ab771dafb43b382615fdd73d0875daa66
Amir Harrison <76721355+aharri64@users.noreply.github.com>
2021-09-20 12:20:14 -0600

    strings

---
+++ Section_2-DataTypesAndVariables/strings/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string myname = "Amir";
+
+            string message = "My name is " + myname;
+
+            string capsMessage = message.ToUpper();
+
+            string lowerCaseMessage = message.ToLower();
+
+
+            Console.WriteLine(message);
+            //Console.WriteLine(capsMessage);
+            //Console.WriteLine(lowerCaseMessage);
+            Console.Read();
+        }
+        
+    }
+}
