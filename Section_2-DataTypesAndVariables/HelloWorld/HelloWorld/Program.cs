30802b9fee6a93955203b2e668c728d6f7d986fc
Amir Harrison <76721355+aharri64@users.noreply.github.com>
2021-09-20 11:58:26 -0600

    data types

--- Section_2-DataTypesAndVariables/HelloWorld/HelloWorld/Program.cs
+++ Section_2-DataTypesAndVariables/HelloWorld/HelloWorld/Program.cs
@@ -7,7 +7,13 @@ namespace HelloWorld
 
         static void Main(string[] args)
         {
+            // declaring a variable
+            int num1;
+
+            // assigning(initializing) a value to a variable
             int num1 = 13;
+
+            // declaring and initializing on the same line
             int num2 = 5;
             int sum = num1 + num2;
 
@@ -15,6 +21,10 @@ namespace HelloWorld
             double d2 = 1.337;
             double sumD = d1 / d2;
 
+            float f1 = 3.1415f;
+            float f2 = 5.1f;
+
+            // Using concatenation 
             Console.WriteLine("The sum of " + num1 + " and " + num2 + " is " + sum);
             Console.WriteLine(d1 + " devided by " + num2 + " is " + sumD);
             Console.Read();
