68b33ea1e9015a24891d44aa493d1aabb0e53859
Amir Harrison <76721355+aharri64@users.noreply.github.com>
2021-10-19 22:18:48 -0600

    exercise 1

---
+++ Section_2-DataTypesAndVariables/strings/Exercise1-strings/Exercise1-strings/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Exercise1_strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter your name and press enter: ");
+            string readInput = Console.ReadLine();
+            Console.WriteLine(readInput.ToUpper());
+            Console.WriteLine(readInput.ToLower());
+            Console.WriteLine(readInput.Trim());
+            Console.WriteLine(readInput.Substring(2, 6));
+        }
+    }
+}
