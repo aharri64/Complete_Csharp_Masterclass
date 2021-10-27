abc6a96f0be42f83da6a521112141f095808a470
Amir Harrison <76721355+aharri64@users.noreply.github.com>
2021-10-26 21:28:03 -0600

    void methods

---
+++ Section_3-Functions-and-Methods/Demos/VoidMethods/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace HelloWorld
+{   
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            WriteSomething();
+            WriteSomethingSpecific("I am an argument and am called from a method");
+            Console.Read();
+        }
+        // access modifier (static) return type method name (parameter1, parameter2)
+        public static void WriteSomething()
+        {
+            Console.WriteLine("I am called from a method");
+        }
+
+        public static void WriteSomethingSpecific(string myText)
+        {
+            Console.WriteLine(myText);
+        }
+
+    }
+}
