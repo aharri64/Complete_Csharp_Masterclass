91e66284d2a488f4119c16ae41c87ba8b2a83d56
Amir Harrison <76721355+aharri64@users.noreply.github.com>
2021-11-02 21:33:40 -0600

    intro to decision making

---
+++ Section_4-Making_Decisions/intoductioToDecisionMakinginCS/intoductioToDecisionMakinginCS/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace intoductioToDecisionMakinginCS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("What is the temperature outside: ");
+            int temperature = Int32.Parse(Console.ReadLine());
+
+            if (temperature < 50)
+            {
+                Console.WriteLine("Take a Coat");
+            }
+            else if (temperature == 50)
+            {
+                Console.WriteLine("It is 50 degrees");
+            }
+            else
+            {
+                Console.WriteLine("It is nice and warm");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Section_4-Making_Decisions/notes.md
@@ -0,0 +1,35 @@
+# Decisions in C#
+
+## If Statement
+
+```cs
+if(condition)
+{
+    //what to do if condition is met
+}
+```
+
+```cs
+if(condition){
+    //what to do if condition is met
+}
+else
+{
+    // what to do if condition is not met
+}
+```
+
+```cs
+if(condition)
+{
+    //what to do if condition is met
+}
+else if (condition)
+{
+    // what to do if condition2 is met
+}
+else
+{
+    // what to do if condition is not met
+}
+```
