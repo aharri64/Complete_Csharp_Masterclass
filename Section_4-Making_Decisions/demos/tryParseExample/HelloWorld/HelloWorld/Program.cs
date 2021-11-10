5edbd3bb16067c57416d88f85fe1dd93ff858632
Amir Harrison <76721355+aharri64@users.noreply.github.com>
2021-11-10 11:47:34 -0700

    tryparse demo

---
+++ Section_4-Making_Decisions/demos/tryParseExample/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace HelloWorld
+{   
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What's the temperature like?");
+            string temperature = Console.ReadLine();
+            int numTemp = int.Parse(temperature);
+            
+            if(numTemp < 20)
+            {
+                Console.WriteLine("Take the coat");
+            }
+
+            if(numTemp == 20)
+            {
+                Console.WriteLine("Pants and Pull Over should be fine");
+            }
+
+            if(numTemp > 20)
+            {
+                Console.WriteLine("Shorts are enough today");
+            }
+
+            Console.Read();
+        }
+
+        
+
+
+    }
+}
