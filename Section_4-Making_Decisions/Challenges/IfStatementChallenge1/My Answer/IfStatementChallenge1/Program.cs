32d71101bdd957ae4ecfc11ee3ef3f220ca3e256
Amir Harrison <76721355+aharri64@users.noreply.github.com>
2021-11-12 12:43:20 -0700

    my answer

---
+++ Section_4-Making_Decisions/Challenges/IfStatementChallenge1/My Answer/IfStatementChallenge1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace IfStatementChallenge1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string password = "password";
+            string username = "Batman5";
+
+            Console.WriteLine("Please enter your username");
+            string attemptedUsername = Console.ReadLine();
+            Console.WriteLine("Please enter your password");
+            string attemptedPassword = Console.ReadLine();
+
+            if (attemptedUsername == username && attemptedPassword == password)
+            {
+                Console.WriteLine($"Welcome {username}");
+            }
+            else
+            {
+                Console.WriteLine("You have entered in a wrong username and/or password");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
