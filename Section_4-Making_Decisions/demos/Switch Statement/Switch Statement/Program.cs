ecaa8d683a0dc0c66df12bed15045f1c1e6f17e2
Amir Harrison <76721355+aharri64@users.noreply.github.com>
2021-11-12 13:32:39 -0700

    switch statment

---
+++ Section_4-Making_Decisions/demos/Switch Statement/Switch Statement/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Switch_Statement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int age = 19;
+
+            switch (age)
+            {
+                case 15:
+                    Console.WriteLine("Too young to party in the club!");
+                    break;
+                case 25:
+                    Console.WriteLine("Good to go!");
+                    break;
+                default:
+                    Console.WriteLine("How old are you then?");
+                    break;
+            }
+
+            if (age == 15)
+            {
+                Console.WriteLine("Too young to party in the club!");
+            }
+            else if (age == 25)
+            {
+                Console.WriteLine("Good to go!");
+            }
+            else
+            {
+                Console.WriteLine("How old are you then?");
+            }
+
+
+
+
+
+
+
+            if (age == 15)
+            {
+                Console.WriteLine("Too young to party in the club!");
+            }
+            else if (age == 25)
+            {
+                Console.WriteLine("Good to go!");
+            }
+            else
+            {
+                Console.WriteLine("How old are you then?");
+            }
+
+            string username = "Frank";
+
+            switch (username)
+            {
+                case "Denis":
+                    Console.WriteLine("username is Denis");
+                    break;
+                case "root":
+                    Console.WriteLine("username is root");
+                    break;
+                default:
+                    Console.WriteLine("username is unknown");
+                    break;
+            }
+
+
+
+            Console.Read();
+        }
+    }
+}
