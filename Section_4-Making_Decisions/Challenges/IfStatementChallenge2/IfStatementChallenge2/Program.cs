68b44f5a444ebbb1f31b6c57f8343c5697a7420d
Amir Harrison <76721355+aharri64@users.noreply.github.com>
2021-11-12 17:34:05 -0700

    if challenge 2 my answer

---
+++ Section_4-Making_Decisions/Challenges/IfStatementChallenge2/IfStatementChallenge2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace IfStatementChallenge2
+{
+    class Program
+    {
+        static int highScore = 300;
+        static string highScorePlayer = "Amir";
+
+        static void Main(string[] args)
+        {
+            PlayerScore(250, "Stormy");
+            PlayerScore(400, "Charlee");
+            PlayerScore(500, "Amir");
+            PlayerScore(500, "Charlee");
+        }
+
+        public static void PlayerScore(int score, string playerName)
+        {
+            
+            if (score > highScore)
+            {
+                highScore = score;
+                highScorePlayer = playerName;
+                Console.WriteLine($"The new high score is {highScore} by {highScorePlayer}");
+            }
+            else if (score == highScore)
+            {
+                Console.WriteLine($"{playerName} scored the same score as {highScorePlayer}");
+            }
+            else
+            {
+                Console.WriteLine($"{playerName}'s score was not high enough, try again");                
+            }
+        }
+    }
+}
