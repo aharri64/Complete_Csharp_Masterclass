9680592a2e82d7a5deb559d967ec2828bfc069ee
Amir Harrison <76721355+aharri64@users.noreply.github.com>
2021-11-19 10:59:39 -0700

    ternary operators

---
+++ Section_4-Making_Decisions/demos/Enhanced If-Statements/Enhanced If-Statements/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enhanced_If_Statements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //[condition] ? [first expression] : [second expression]
+            //condition has to be either true or false
+
+            //The conditional Operator is right-associative
+            //That means, that a ? b : c ? d : e
+            // is evaluated as a ? b : (c ? d : e)
+
+            //The conditional Operator cannot be overloaded.
+
+            //in celcius
+            //Play around with that Value
+            int temperature = -5;
+
+            //Our result
+            string stateOfMatter;
+            
+            //Is the Temperature below 0 Degree Celsius?
+            if(temperature < 0)
+            {
+                stateOfMatter = "solid";
+            }
+            //Is the Temperature higher than 100 Degree Celcius?
+            else if(temperature > 100)
+            {
+                stateOfMatter = "gas";
+            }
+            //In all other Cases it must be liquid.
+            else
+            {
+                stateOfMatter = "liquid";
+            }
+
+            Console.WriteLine("State of Matter is {0}", stateOfMatter);
+
+            //The same result can be achieved by this
+            //The enhanced Version of the If-Else-Statement from above
+            stateOfMatter = temperature < 0 ? "solid" : (temperature > 100 ? "gas" : "liquid");
+
+            Console.WriteLine("State of Matter is {0}", stateOfMatter);
+
+            //To make sure the Console keeps open
+            Console.Read();
+
+        }
+    }
+}
--- Section_4-Making_Decisions/notes.md
+++ Section_4-Making_Decisions/notes.md
@@ -112,3 +112,56 @@ static void Main(string[] args)
 
 Parsing fails when the string can not be converted into the desired data type.
 if a letter or character is used for example.
+
+# Enhanced if Statements - Ternary Operators
+
+```cs
+//=[condition] ? [first expression] : [second expression]
+
+//condition has to be either true or false
+
+//The conditional Operator is right-associative
+
+//That means, that a ? b : c ? d : e
+
+// is evaluated as a ? b : (c ? d : e)
+
+//The conditional Operator cannot be overloaded.
+
+//in celcius
+//Play around with that Value
+```
+
+```cs
+int temperature = -5;
+
+//Our result
+string stateOfMatter;
+
+//Is the Temperature below 0 Degree Celsius?
+if(temperature < 0)
+{
+    stateOfMatter = "solid";
+}
+//Is the Temperature higher than 100 Degree Celcius?
+else if(temperature > 100)
+{
+    stateOfMatter = "gas";
+}
+//In all other Cases it must be liquid.
+else
+{
+    stateOfMatter = "liquid";
+}
+
+Console.WriteLine("State of Matter is {0}", stateOfMatter);
+
+//The same result can be achieved by this
+//The enhanced Version of the If-Else-Statement from above
+stateOfMatter = temperature < 0 ? "solid" : (temperature > 100 ? "gas" : "liquid");
+
+Console.WriteLine("State of Matter is {0}", stateOfMatter);
+
+//To make sure the Console keeps open
+Console.Read();
+```
