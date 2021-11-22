60ba1a2043083cfa163669ea0c9ed0ab617efed3
Amir Harrison <76721355+aharri64@users.noreply.github.com>
2021-11-22 16:44:43 -0700

    break and continue

---
+++ Section_5-Loops/Demos/breakAndContinue/BreakContinue/BreakContinue/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BreakContinue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for(int counter = 0; counter< 10; counter++)
+            {
+                
+                if(counter %2 ==0)
+                {
+                    Console.WriteLine("Now comes an odd number!");
+                    continue;
+                }
+                Console.WriteLine(counter);
+            }
+            Console.Read();
+
+        }
+    }
+}
