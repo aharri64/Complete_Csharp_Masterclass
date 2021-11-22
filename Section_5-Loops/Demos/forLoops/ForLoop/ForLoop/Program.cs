388e9128f07ad366e85c261dfad60b1c82be52bf
Amir Harrison <76721355+aharri64@users.noreply.github.com>
2021-11-22 16:42:39 -0700

    for loops

---
+++ Section_5-Loops/Demos/forLoops/ForLoop/ForLoop/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int counter = 1; counter < 20; counter += 2)
+            {
+                Console.WriteLine(counter);
+            }
+            Console.Read();
+
+        }
+    }
+}
