afb278640bec59614e4aea3b0086129a8775dfe5
Amir Harrison <76721355+aharri64@users.noreply.github.com>
2021-11-22 16:43:54 -0700

    while loops

---
+++ Section_5-Loops/Demos/whileLoops/WhileLoop/WhileLoop/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhileLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 0;
+            string enteredText = "";
+            while(enteredText.Equals(""))
+            {
+                Console.WriteLine("Please press enter to increase amount by one and anything else" +
+                    " + enter if you want to finish counting");
+                enteredText = Console.ReadLine();
+                
+                counter++;
+                Console.WriteLine("Current people count is {0}", counter);
+
+            }
+            Console.WriteLine("{0} people are inside the bus. Press enter to close the program", counter);
+            Console.Read();
+        }
+    }
+}
