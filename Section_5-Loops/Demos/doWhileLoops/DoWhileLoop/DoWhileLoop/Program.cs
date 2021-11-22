cd1eaf699e56849c96a5f127617e0d73d50c3d7c
Amir Harrison <76721355+aharri64@users.noreply.github.com>
2021-11-22 16:43:17 -0700

    dowhile

---
+++ Section_5-Loops/Demos/doWhileLoops/DoWhileLoop/DoWhileLoop/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoWhileLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int lenghtOfText = 0;
+            string wholeText = "";
+            do
+            {
+                Console.WriteLine("Please enter the name of a friend");
+                string nameOfAFriend = Console.ReadLine();
+                int currentLenght = nameOfAFriend.Length;
+                lenghtOfText += currentLenght;
+                wholeText += nameOfAFriend;
+            } while (lenghtOfText < 20);
+            Console.WriteLine("Thanks, that was enough! " + wholeText);
+            Console.Read();
+        }
+    }
+}
