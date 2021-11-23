f2e0a6da820c27ef11d206307b2b87c7f2d0e302
Amir Harrison <76721355+aharri64@users.noreply.github.com>
2021-11-22 19:08:22 -0700

    dowhileloop

--- MainNotes.md
+++ MainNotes.md
@@ -906,3 +906,30 @@ namespace ForLoop
     }
 }
 ```
+
+## Do While Loops
+
+```cs
+namespace DoWhileLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int lenghtOfText = 0;
+            string wholeText = "";
+            do
+            {
+                Console.WriteLine("Please enter the name of a friend");
+                string nameOfAFriend = Console.ReadLine();
+                int currentLenght = nameOfAFriend.Length;
+                lenghtOfText += currentLenght;
+                wholeText += nameOfAFriend;
+            } while (lenghtOfText < 20);
+            Console.WriteLine("Thanks, that was enough! " + wholeText);
+            Console.Read();
+        }
+    }
+}
+```
--- Section_5-Loops/Demos/doWhileLoops/DoWhileLoop/DoWhileLoop/Program.cs
+++ Section_5-Loops/Demos/doWhileLoops/DoWhileLoop/DoWhileLoop/Program.cs
@@ -11,17 +11,19 @@ namespace DoWhileLoop
         static void Main(string[] args)
         {
 
-            int lenghtOfText = 0;
             string wholeText = "";
+            int lengthOfText = 0;
             do
             {
-                Console.WriteLine("Please enter the name of a friend");
+                Console.WriteLine("Please enter the name of a friend: ");
                 string nameOfAFriend = Console.ReadLine();
-                int currentLenght = nameOfAFriend.Length;
-                lenghtOfText += currentLenght;
+                int currentLength = nameOfAFriend.Length;
+                lengthOfText += currentLength;
                 wholeText += nameOfAFriend;
-            } while (lenghtOfText < 20);
-            Console.WriteLine("Thanks, that was enough! " + wholeText);
+            } while (lengthOfText <= 20);
+
+            Console.WriteLine("Thanks, that was enough " + wholeText);
+
             Console.Read();
         }
     }
