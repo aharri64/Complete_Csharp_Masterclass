96d2c595cad6020ea9f9b7f0745e5247921b1f66
Amir Harrison <76721355+aharri64@users.noreply.github.com>
2021-11-22 19:20:42 -0700

    while challenge

--- Section_5-Loops/Demos/whileLoops/WhileLoop/WhileLoop/Program.cs
+++ Section_5-Loops/Demos/whileLoops/WhileLoop/WhileLoop/Program.cs
@@ -11,18 +11,17 @@ namespace WhileLoop
         static void Main(string[] args)
         {
             int counter = 0;
-            string enteredText = "";
-            while(enteredText.Equals(""))
+            string entry = "";
+            while (entry == "")
             {
-                Console.WriteLine("Please press enter to increase amount by one and anything else" +
-                    " + enter if you want to finish counting");
-                enteredText = Console.ReadLine();
-                
-                counter++;
-                Console.WriteLine("Current people count is {0}", counter);
+                Console.WriteLine("Please enter something: ");
+                entry = Console.ReadLine();
 
+                counter++;
+                Console.WriteLine($"Current count is {counter}");
             }
-            Console.WriteLine("{0} people are inside the bus. Press enter to close the program", counter);
+
+            Console.WriteLine("You entered a character, game over.");
             Console.Read();
         }
     }
