5acf4839ccb9be570162b944afd844806532b878
Amir Harrison <76721355+aharri64@users.noreply.github.com>
2021-11-29 11:49:11 -0700

    challenge, didn't figure out.

---
+++ Section_5-Loops/Challenge-Loops1-Average/Challenge-Loops1-Average/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Challenge_Loops1_Average
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "0";
+            int count = 0;
+            int currentNumber = 0;
+            int total = 0;
+
+            while (input != "-1")
+            {
+                Console.WriteLine($"Last number was {currentNumber}");
+                Console.WriteLine("Please enter the next score");
+                Console.WriteLine($"Current amount of entries {count}");
+                Console.WriteLine("Please enter -1 when you are finished entering scores");
+
+                input = Console.ReadLine();
+                if (input == "-1")
+                {
+                    Console.WriteLine("---------------------------------------------------");
+                    double average = (double)total / (double)count;
+                    Console.WriteLine($"The average score of your students is {average}");
+                }
+
+                if (int.TryParse(input, out currentNumber) && currentNumber > 0 && currentNumber < 21)
+                {
+                    total += currentNumber;
+                }
+                else
+                {
+                    if (!(input.Equals("-1")))
+                    {
+                        Console.WriteLine("Please enter a value between 1 and 20!");
+                    }
+
+                    continue;
+                }
+
+                count++;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
