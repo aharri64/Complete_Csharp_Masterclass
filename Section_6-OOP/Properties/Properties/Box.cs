cdc45f56dd48e5db8e3aba50ef2540eebf70a1ac
Amir Harrison <76721355+aharri64@users.noreply.github.com>
2022-01-14 20:38:03 -0700

    properties part 1

---
+++ Section_6-OOP/Properties/Properties/Box.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Properties
+{
+    class Box
+    {
+        //member variables
+        private int length = 3;
+        public int height;
+        public int width;
+        public int volume;
+
+        public void SetLength(int length)
+        {
+            if (length <= 0)
+            {
+                throw new Exception("Length should be higher that 0");
+            }
+            this.length = length;
+        }
+
+        public int GetLength()
+        {
+            return this.length;
+        }
+
+        public int GetVolume()
+        {
+            return this.height * this.length * this.width;
+        }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Length is {length}, height is {height} and width is " +
+                              $"{width} so the volume is {GetVolume()}");
+        }
+
+
+    }
+}
---
+++ Section_6-OOP/Properties/Properties/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Properties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Box box = new Box();
+            //box.length = 3;
+            box.SetLength(0);
+            box.height = 4;
+            box.width = 5;
+
+            box.DisplayInfo();
+        }
+    }
+}
