592776f127f46bc051e838ac55f0ff745431e7a8
Amir Harrison <haveacompleteday@gmail.com>
2022-01-19 10:33:48 -0700

    added arrays

---
+++ Section_7-Collections/07 - Arrays/DeclaringAndInitializing/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07___Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // declare and initialize array grades
+            int[] grades = new int[5];
+
+            grades[0] = 20;
+            grades[1] = 15;
+            grades[2] = 12;
+            grades[3] = 9;
+            grades[4] = 7;
+
+            Console.WriteLine("grades at index 0 : {0}", grades[0]);
+
+            string input = Console.ReadLine();
+            // assign value to array grades at index 0
+            grades[0] = int.Parse(input);
+            Console.WriteLine("grades at index 0 : {0}", grades[0]);
+            
+
+            // another way of initializing an array
+            int[] gradesOfMathStudentsA = { 20, 13, 12, 8, 8 };
+
+            // thrid way of initializing an array
+            int[] gradesOfMathStudentsB = new int[] { 15, 20, 3, 17, 18, 15 };
+
+            Console.WriteLine("Length of gradesOfMathStudentsA: {0}", gradesOfMathStudentsA.Length);
+            Console.ReadKey();
+        }
+    }
+}
